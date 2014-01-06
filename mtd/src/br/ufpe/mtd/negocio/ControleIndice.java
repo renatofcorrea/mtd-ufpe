4acd01441291043c15673d3424535272d62a505c
djalmaafilho <djalmaafilho@gmail.com>
2014-01-06 11:33:54 +0000

    [ issue 3 ] otimizar indice.

--- mtd/src/br/ufpe/mtd/negocio/ControleIndice.java
+++ mtd/src/br/ufpe/mtd/negocio/ControleIndice.java
@@ -2,12 +2,15 @@ package br.ufpe.mtd.negocio;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import br.ufpe.mtd.consulta.OAIPMHDriver;
 import br.ufpe.mtd.dados.RepositorioIndice;
 import br.ufpe.mtd.entidade.Identificador;
+import br.ufpe.mtd.excecao.MTDException;
 import br.ufpe.mtd.thread.BaseThread;
 import br.ufpe.mtd.thread.ThreadBuscaMetadados;
 import br.ufpe.mtd.util.MTDFactory;
+import br.ufpe.mtd.util.MTDUtil;
 
 public class ControleIndice {
 	private final  int QTD_MAX_TENTATIVAS = 2;
@@ -23,19 +26,27 @@ public class ControleIndice {
 		List<Identificador> identificadores = null;
 		List<Identificador> deletados = new ArrayList<Identificador>();
 		OAIPMHDriver driver = new  OAIPMHDriver(urlBase, metaDataPrefix);
-		
-		while(driver.hasNext()){
-			tentativas++;
-			
-			try{
-				identificadores = driver.getNextIdentifiers();
-				
+		try {
+			while(driver.hasNext()){
+				try{
+					tentativas++;
+					identificadores = driver.getNextIdentifiers();
+					tentativas = 0;
+				}catch(Exception e){
+					if((e instanceof java.net.SocketTimeoutException  || e instanceof java.net.SocketException) &&
+							tentativas > QTD_MAX_TENTATIVAS){
+						 
+						MTDFactory.getInstancia().getLog().salvarDadosLog(new MTDException(e, "Exceção apos Tentativas ("+tentativas+")"));
+						throw e;
+					}else{
+						throw e;
+					}
+				}
+					
 				//tratamento pois dados de identificadores podem vir repetidos 
 				//TODO: verificar se este comportamento de repticao pode ser evitado.
 				if(identificadores.size() > 0 && !dadosRecebidos.contains(identificadores.get(0))){
-					
 					dadosRecebidos.addAll(identificadores);
-					
 					for (Identificador identificador : identificadores) {
 						if(identificador.isDeletado()){
 							deletados.add(identificador);
@@ -50,24 +61,15 @@ public class ControleIndice {
 					deletados.clear();
 					baixarDocsEsalvar(repositorio, identificadores, urlBase, metaDataPrefix);
 				}
-				
-				tentativas = 0;
-				
-			}catch(Exception e){
-				
-				if((e instanceof java.net.SocketTimeoutException  || e instanceof java.net.SocketException) &&
-						tentativas > QTD_MAX_TENTATIVAS){
-					MTDFactory.getInstancia().getLog().salvarDadosLog(e);
-					throw e;
-				}else{
-					throw e;
-				}
-				
-			}finally{
-				otimizarIndice(repositorio);
 			}
+			
+		} finally {
+			MTDFactory.getInstancia().getPoolThread().aguardarFimDoPool();
+			//se ocorrer excecao sobe para metodo chamador, porem sempre otimiza o indice antes.
+			otimizarIndice(repositorio);
 		}
 	}
+
 	
 	/**
 	 * Realiza a coleta registros de forma online
@@ -77,10 +79,10 @@ public class ControleIndice {
 	 * @param identificadores
 	 * @param urlBase
 	 * @param metaDataPrefix
+	 * @throws InterruptedException 
 	 */
-	private void baixarDocsEsalvar(RepositorioIndice repositorio, List<Identificador> identificadores,String urlBase,String metaDataPrefix){
+	private void baixarDocsEsalvar(RepositorioIndice repositorio, List<Identificador> identificadores,String urlBase,String metaDataPrefix) throws InterruptedException{
 		ThreadBuscaMetadados t = new ThreadBuscaMetadados(repositorio , identificadores,urlBase,metaDataPrefix);
-		t.setPriority(Thread.MAX_PRIORITY);
 		t.executarNoPool();
 	}
 	
@@ -89,23 +91,20 @@ public class ControleIndice {
 	 * Agenda a otimizacao do indice.
 	 * Deve ser chamado apos todos os documentos
 	 * estarem agendados para serem inseridos no indice.
-	 * Nao e prioritario que seja a ultima thread  a ser executada na fila
-	 * por isso nao foi preciso um controle rigido nesse sentido.
-	 * Apenas baixou-se a prioridade desta execucao em relacao as demais.
 	 */
 	private void otimizarIndice(final RepositorioIndice repositorio){
 		BaseThread t = new BaseThread(){
 			public void run() {
 				try {
+					MTDUtil.imprimirConsole("Iniciando otimização do indice....");
 					repositorio.otimizarIndice();
-					
+					MTDUtil.imprimirConsole("...Concluida otimização do indice");					
 				} catch (Exception e) {
 					MTDFactory.getInstancia().getLog().salvarDadosLog(e);
 				}
 			};
 		};
 		
-		t.setPriority(Thread.MIN_PRIORITY);
 		t.executarNoPool();
 	}
 }
