11147c739fff168491e7b946aa8f022641f93604
djalmaafilho@gmail.com <djalmaafilho@gmail.com@83800bff-844b-325f-aaee-6b350ed329d2>
2014-01-20 21:18:52 +0000

    [ issue 10 ] Criar estratégia para minimizar os erros de Timeout

--- trunk/mtd/src/br/ufpe/mtd/negocio/ControleIndice.java
+++ trunk/mtd/src/br/ufpe/mtd/negocio/ControleIndice.java
@@ -6,14 +6,13 @@ import java.util.List;
 import br.ufpe.mtd.consulta.OAIPMHDriver;
 import br.ufpe.mtd.dados.IRepositorioIndice;
 import br.ufpe.mtd.entidade.Identificador;
-import br.ufpe.mtd.excecao.MTDException;
 import br.ufpe.mtd.thread.BaseThread;
 import br.ufpe.mtd.thread.ThreadBuscaMetadados;
 import br.ufpe.mtd.util.MTDFactory;
 import br.ufpe.mtd.util.MTDUtil;
 
 public class ControleIndice {
-	private final  int QTD_MAX_TENTATIVAS = 2;
+
 	private IRepositorioIndice repositorio;
 
 	public ControleIndice(IRepositorioIndice repositorio) {
@@ -21,7 +20,7 @@ public class ControleIndice {
 	}
 	
 	public void indexar(String urlBase, String metaDataPrefix) throws Exception {
-		int tentativas = 0;
+		
 		List<Identificador> dadosRecebidos = new ArrayList<Identificador>();
 		List<Identificador> identificadores = null;
 		List<Identificador> deletados = new ArrayList<Identificador>();
@@ -29,20 +28,7 @@ public class ControleIndice {
 		try {
 			while(driver.hasNext()){
 				
-				try{
-					tentativas++;
-					identificadores = driver.getNextIdentifiers();
-					tentativas = 0;
-				}catch(Exception e){
-					if((e instanceof java.net.SocketTimeoutException  || e instanceof java.net.SocketException) &&
-							tentativas > QTD_MAX_TENTATIVAS){
-						 
-						MTDFactory.getInstancia().getLog().salvarDadosLog(new MTDException(e, "Exceção apos Tentativas ("+tentativas+")"));
-						throw e;
-					}else{
-						throw e;
-					}
-				}
+				identificadores = driver.getNextIdentifiers();
 					
 				//tratamento pois dados de identificadores podem vir repetidos 
 				//TODO: verificar se este comportamento de repticao pode ser evitado.
@@ -70,7 +56,6 @@ public class ControleIndice {
 			otimizarIndice(repositorio);
 		}
 	}
-
 	
 	/**
 	 * Realiza a coleta registros de forma online
